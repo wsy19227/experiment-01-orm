bd4ad08b1e0b54e029c0d262a52294f809b7f092
wsy19227 <48052401+wsy19227@users.noreply.github.com>
2019-03-15 23:41:07 +0800

    Update ElectiveRepository.java

--- src/main/java/com/example/springbootexperiment01/repository/ElectiveRepository.java
+++ src/main/java/com/example/springbootexperiment01/repository/ElectiveRepository.java
@@ -13,34 +13,33 @@ public class ElectiveRepository {
     @PersistenceContext
     private EntityManager em;
 
-    public void addElective() {
-        Student student = new Student("思");
+    public void addStudentAndCourse() {
+        Student student = new Student("李白");
         em.persist(student);
-        Student student1 = new Student("昭");
+        Student student1 = new Student("杜甫");
         em.persist(student1);
 
-        Course course = new Course("设计模式");
+        Course course = new Course("数学");
         em.persist(course);
-        Course course1 = new Course("xml");
+        Course course1 = new Course("英语");
         em.persist(course1);
+    }
 
-        var s1=em.find(Student.class,11);
-        var c1=em.find(Course.class,12);
-
-        var s2=em.find(Student.class,12);
-        var c2=em.find(Course.class,11);
+    public void addElective(){
+        var s1=em.find(Student.class,13);
+        var c1=em.find(Course.class,14);
 
         Elective elective=new Elective();
         elective.setStudent(s1);
         elective.setCourse(c1);
         em.persist(elective);
 
+        var s2=em.find(Student.class,14);
+        var c2=em.find(Course.class,13);
+
         Elective elective1=new Elective();
         elective1.setStudent(s2);
         elective1.setCourse(c2);
-
         em.persist(elective1);
-
-
     }
 }
